b9aaaf841c7b725394bcceb0b2f19e944596ea2a
fdebertranddeb01 <fdebertranddeb01@SEC-C210-F21.in.insa-strasbourg.fr>
2026-09-08 17:37:13 +0200

    seance2

--- src/main/java/fr/insa/beuvron/cours/multiTache/pasApas2026/threadBasic/PairThread.java
+++ src/main/java/fr/insa/beuvron/cours/multiTache/pasApas2026/threadBasic/PairThread.java
@@ -10,20 +10,41 @@ package fr.insa.beuvron.cours.multiTache.pasApas2026.threadBasic;
  */
 public class PairThread extends Thread {
     
+    public PairThread(String name) {
+        super(name);
+    }
+
     @Override
     public void run() {
-        for (int i = 0 ; i < 10 ; i += 2) {
+        for (int i = 0; i < 10; i += 2) {
+            Thread cur = Thread.currentThread();
+            System.out.println("Thead courant : "+cur.getName());
+            try {
+                Thread.sleep((int) (Math.random()*5000));
+            } catch (InterruptedException ex) {
+                throw new Error("impossible" , ex);
+            }
             System.out.println(i);
         }
-        
+
     }
-    
+
     public static void main(String[] args) {
-        PairThread p1 = new PairThread();
+        PairThread p1 = new PairThread("t1");
         p1.start();
-        PairThread p2 = new PairThread();
+        PairThread p2 = new PairThread("t2");
         p2.start();
+        try {
+            p1.join();
+        } catch (InterruptedException ex) {
+            throw new Error("normalement impossible", ex);
+        }
+        try {
+            p2.join();
+        } catch (InterruptedException ex) {
+            throw new Error("normalement impossible", ex);
+        }
         System.out.println("fini");
     }
-    
+
 }
